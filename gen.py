d35553460fd1b490765e4f30d42716a4207deb6c
Joshua Salisbury <joshua.l.salisbury@gmail.com>
2018-02-09 17:28:41 -0600

    first working loaded model

--- gen.py
+++ gen.py
@@ -89,19 +89,38 @@ for i in range(rings - 1):
         elements.append([n1, len(nodes) - 1, n3])
 
 # roots
-for n in [32, 36, 40, 44]:
-    r12dl = math.sqrt(nodes[n][0] ** 2 + nodes[n][1] ** 2)
-    r12dn = [nodes[n][0] / r12dl * r_mid, nodes[n][1] / r12dl * r_mid, 0]
-    r1m = [(nodes[n][0] + r12dn[0]) / 2, (nodes[n][1] + r12dn[1]) / 2, (nodes[n][2] + r12dn[2]) / 2]
-    nodes.append(r1m)
-    nodes.append(r12dn)
-    elements.append([n, len(nodes) - 2, len(nodes) - 1])
+for n in [33, 37, 41, 45]:
+    r2dl = math.sqrt(nodes[n-1][0] ** 2 + nodes[n-1][1] ** 2) # root 2D length
+    r2dn = [nodes[n-1][0] / r2dl * r_mid, nodes[n-1][1] / r2dl * r_mid, 0] # root 2D end node
+    rm = [(nodes[n-1][0] + r2dn[0]) / 2, (nodes[n-1][1] + r2dn[1]) / 2, (nodes[n-1][2] + r2dn[2]) / 2] # root midpoint
+    nodes.append(rm)
+    nodes.append(r2dn)
+    elements.append([n-1, len(nodes) - 2, len(nodes) - 1])
 
 # midgard
-p1 = nodes[68]
-p2 = nodes[74]
-p3 = [-r_mid, 0, nodes[68][2]]
+z_mid = nodes[68][2]
+for n in [69, 71, 73, 75]:
+    mn = [-r_mid, nodes[n-1][1], z_mid]
+    mm = [(nodes[n-1][0] - r_mid) / 2, nodes[n-1][1], z_mid]
+    nodes.append(mm)
+    nodes.append(mn)
+    elements.append([n-1, len(nodes) - 2, len(nodes) - 1])
+for i in [(282, 284), (284, 286), (286, 288)]:
+    x_mid = (nodes[i[0]-1][0] + nodes[i[1]-1][0]) / 2
+    y_mid = (nodes[i[0]-1][1] + nodes[i[1]-1][1]) / 2
+    z_mid = (nodes[i[0]-1][2] + nodes[i[1]-1][2]) / 2
+    s_mid = [x_mid, y_mid, z_mid]
+    nodes.append(s_mid)
+    elements.append([i[0]-1, len(nodes) - 1, i[1]-1])
 
+# midgard supports
+for i in [(281, 55), (283, 55), (283, 57), (285, 57), (285, 59), (287, 59)]:#, (282, 37), (284, 39), (286, 41), (288, 43)]:
+    x_mid = (nodes[i[0]-1][0] + nodes[i[1]-1][0]) / 2
+    y_mid = (nodes[i[0]-1][1] + nodes[i[1]-1][1]) / 2
+    z_mid = (nodes[i[0]-1][2] + nodes[i[1]-1][2]) / 2
+    s_mid = [x_mid, y_mid, z_mid]
+    nodes.append(s_mid)
+    elements.append([i[0]-1, len(nodes) - 1, i[1]-1])
 
 fout.write('*NODE,NSET=Nall\n')
 for i, node in enumerate(nodes):
@@ -116,3 +135,33 @@ for i, el in enumerate(elements):
     el_line = str(i + 1) + ', ' + str(el[0] + 1) + ', ' + str(el[1] + 1) + ', ' + str(el[2] + 1) + '\n'
 
     fout.write(el_line)
+
+fout.write('*BOUNDARY\n')
+for i in [1,3,5,7,9,11,13,15,274,276,278,280]:
+    fout.write(str(i) + ',1,3\n')
+
+fout.write('*MATERIAL,NAME=WOOD\n')
+fout.write('*ELASTIC\n')
+fout.write('2.5E8,.038\n')
+
+fout.write('*BEAM SECTION,ELSET=EAll,MATERIAL=WOOD,SECTION=RECT\n')
+fout.write('.125,.292\n')
+
+fout.write('*STEP\n')
+fout.write('*STATIC\n')
+fout.write('*CLOAD\n')
+fout.write('282,3,-1.\n')
+fout.write('284,3,-1.\n')
+fout.write('286,3,-1.\n')
+fout.write('288,3,-1.\n')
+
+fout.write('*EL PRINT,ELSET=Eall\n')
+fout.write('S\n')
+
+fout.write('*EL FILE\n')
+fout.write('S\n')
+
+fout.write('*NODE FILE\n')
+fout.write('U\n')
+
+fout.write('*END STEP\n')
